9252b8c2826b15d89732351d3251e6d814e3c054
jv222th <jv222th@student.lnu.se>
2025-08-20 17:50:43 +0200

    Star project and do some quick tests with react, sigma and graphology

---
+++ src/data/genderData.ts
@@ -0,0 +1,5 @@
+/**
+ * @file This module contains the data analysis/processing for gender in movies.
+ * @module genderData
+ * @author Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
+ */
---
+++ src/data/yearData.ts
@@ -0,0 +1,5 @@
+/**
+ * @file This module contains the data analysis/processing for years.
+ * @module yearData
+ * @author Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
+ */
