f8ddeca2301f212bde53020c05cf279a9a0fffae
jv222th <jv222th@student.lnu.se>
2025-08-20 18:16:22 +0200

    Setup the directories and create temporary files (empty) to get started

---
+++ src/api/client.ts
@@ -0,0 +1,5 @@
+/**
+ * @file This module contains a client that fetches data from the backend API.
+ * @module client
+ * @author Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
+ */
---
+++ src/hooks/useMovieStats.ts
@@ -0,0 +1,5 @@
+/**
+ * @file This module contains the hook that fetches and proces
+ * @module graphData
+ * @author Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
+ */
